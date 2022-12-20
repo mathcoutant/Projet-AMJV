1736abb6e896dbc5076a6ec1856c0ffabed3c49d
Mathieu Coutant <mathieu.coutant@telecom-sudparis.eu>
2022-12-20 17:36:18 +0100

    Complete MainScreen, complete SettingsScreen and begining of CharacterScreen

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private GameObject mainMenuContainer;
+    private GameObject settingsContainer;
+    private GameObject characterContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingsContainer = GameObject.Find("SettingsContainer");
+        mainMenuContainer = GameObject.Find("MainMenuContainer");
+        characterContainer = GameObject.Find("ChooseCharacterContainer");
+        SwitchToMainMenuScreen();
+    }
+
+    public void SwitchToMainMenuScreen()
+    {
+        mainMenuContainer.SetActive(true);
+        settingsContainer.SetActive(false);
+        characterContainer.SetActive(false);
+    }
+
+    public void SwitchToSettingsScreen()
+    {
+        mainMenuContainer.SetActive(false);
+        settingsContainer.SetActive(true);
+        characterContainer.SetActive(false);
+    }
+    public void SwitchToCharacterScreen()
+    {
+        mainMenuContainer.SetActive(false);
+        settingsContainer.SetActive(false);
+        characterContainer.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
