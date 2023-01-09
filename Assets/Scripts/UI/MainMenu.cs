cd33bce397f2a5004401897ed9ef1f86b0823a27
Mathieu Coutant <mathieu.coutant2003@gmail.com>
2023-01-09 11:08:23 +0100

    Resolution dropdown

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -14,13 +14,23 @@ public class MainMenu : MonoBehaviour
     public Slider volumeSlider;
     public Slider effectSlider;
     public Slider musicSlider;
+    public TMP_Dropdown resolutionDropdown;
     private GameObject nameDescriptionText;
     private GameObject descriptionText;
     private GameObject victoryIndicator;
+    private Image characterImage;
+    [SerializeField] private Sprite warriorImage = null;
+    [SerializeField] private Sprite mageImage = null;
+    [SerializeField] private Sprite archerImage = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        characterImage = GameObject.Find("CharacterImageContainer").GetComponent<Image>();
+        warriorImage = Resources.Load<Sprite>("Images/CharacterImages/canardtrosad");
+        mageImage = Resources.Load<Sprite>("Images/CharacterImages/logoDiscord");
+        archerImage = Resources.Load<Sprite>("Images/CharacterImages/logoDiscord");
+
         // Setting the text object for the characters descriptions
         nameDescriptionText = GameObject.Find("CharacterNameText");
         descriptionText = GameObject.Find("CharacterDescriptionText");
@@ -35,16 +45,21 @@ public class MainMenu : MonoBehaviour
 
         // Setting the different sliders for the settings screen
         volumeSlider.value = 1;
-        GameObject volumeText = volumeSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject;
+        TextMeshProUGUI volumeText = volumeSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject.GetComponent<TextMeshProUGUI>();
         volumeSlider.onValueChanged.AddListener(delegate { SliderValueChange(volumeSlider, volumeText); });
 
         effectSlider.value = 1;
-        GameObject effectText = effectSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject;
+        TextMeshProUGUI effectText = effectSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject.GetComponent<TextMeshProUGUI>();
         effectSlider.onValueChanged.AddListener(delegate { SliderValueChange(effectSlider, effectText); });
 
         musicSlider.value = 1;
-        GameObject musicText = musicSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject;
+        TextMeshProUGUI musicText = musicSlider.transform.parent.gameObject.transform.Find("Percentage").gameObject.GetComponent<TextMeshProUGUI>();
         musicSlider.onValueChanged.AddListener(delegate { SliderValueChange(musicSlider, musicText); });
+
+        // Add listener to the dropdown
+        resolutionDropdown.onValueChanged.AddListener(delegate {
+            ResolutionDropdownValueChanged();
+        });
     }
 
     public void SwitchToMainMenuScreen()
@@ -76,10 +91,32 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    void SliderValueChange(Slider slider, GameObject textObject)
+    private void SliderValueChange(Slider slider, TextMeshProUGUI textObject)
     {
         // Change the text in the indicator in percentage
-        textObject.GetComponent<TextMeshProUGUI>().text = (slider.value * 100).ToString("F0") +"%";
+        textObject.text = (slider.value * 100).ToString("F0") +"%";
+    }
+
+    private void ResolutionDropdownValueChanged()
+    {
+        switch(resolutionDropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            case 1:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            case 2:
+                Screen.SetResolution(1280, 720, false);
+                break;
+            case 3:
+                Screen.SetResolution(720, 480, false);
+                break;
+            default:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+        }
     }
 
    public void ChangeDescription(int i)
@@ -97,7 +134,10 @@ public class MainMenu : MonoBehaviour
 
         nameDescriptionText.GetComponent<TextMeshProUGUI>().text = tab[0];
         descriptionText.GetComponent<TextMeshProUGUI>().text = desc;
-        if(tab[4] == "True")
+        if (i == 0) { characterImage.sprite = warriorImage; }
+        if (i == 1) { characterImage.sprite = mageImage; }
+        if (i == 2) { characterImage.sprite = archerImage; }
+        if (tab[4] == "True")
         {
             victoryIndicator.SetActive(true);
         } else
