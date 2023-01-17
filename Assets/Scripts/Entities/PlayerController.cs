3ad2269a67f95b2f81bc2e33802d7655240e9eed
Mathieu Coutant <91986882+Thimotay@users.noreply.github.com>
2023-01-17 18:16:17 +0100

    Player movements (#5)
    * camera follows player
    
    * Player movements, rotation and action cooldowns

--- Assets/Scripts/Entities/PlayerController.cs
+++ Assets/Scripts/Entities/PlayerController.cs
@@ -5,41 +5,84 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    private Camera cam;
-    private NavMeshAgent nma;
+    private Rigidbody rigidB;
+    public float speed = 20f;
+
+    // actions options
+    private bool canDoAction1 = true;
+    public float cooldownAction1 = 0.3f;
+    private bool canDoAction2 = true;
+    public float cooldownAction2 = 1f;
+    private bool canDoAction3 = true;
+    public float cooldownAction3 = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = GameObject.FindObjectOfType<Camera>();
-        nma = gameObject.GetComponent<NavMeshAgent>();
+        rigidB = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        MovePlayer();
+        RotatePlayerToMouse();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canDoAction1)
         {
-            MovePlayerToClick();
+            canDoAction1 = false;
+            // Do action 1
+            StartCoroutine(Cooldown(1));
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && canDoAction2)
+        {
+            canDoAction2 = false;
+            // Do action 2
+            StartCoroutine(Cooldown(2));
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && canDoAction3)
+        {
+            canDoAction3 = false;
+            // Do action 3
+            StartCoroutine(Cooldown(3));
         }
     }
 
-    void MovePlayerToClick()
+    void MovePlayer()
     {
-        nma.SetDestination(GetPositionOfClick());
+        float hSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float vSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        rigidB.velocity = new Vector3(hSpeed, 0, -vSpeed);
+    }
+    void RotatePlayerToMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 objPos = Camera.main.WorldToScreenPoint(transform.position);
+        
+        mousePos.x = mousePos.x - objPos.x;
+        mousePos.y = mousePos.y - objPos.y;
+
+        float angle = Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler( new Vector3(0, angle, 0));
     }
 
-    Vector3 GetPositionOfClick()
+    IEnumerator Cooldown(int numAction)
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        switch (numAction)
         {
-            return hit.point;
-        }
-        else
-        {
-            return gameObject.transform.position;
+            case 1:
+                yield return new WaitForSeconds(cooldownAction1);
+                canDoAction1 = true;
+                break;
+            case 2:
+                yield return new WaitForSeconds(cooldownAction2);
+                canDoAction2 = true;
+                break;
+            case 3:
+                yield return new WaitForSeconds(cooldownAction3);
+                canDoAction3 = true;
+                break;
         }
     }
 }
