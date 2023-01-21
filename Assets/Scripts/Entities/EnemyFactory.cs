b9e188b8b6e1462a0fac6cf612e23f408cadf3d5
Mathieu Coutant <mathieu.coutant2003@gmail.com>
2023-01-21 11:36:54 +0100

    spawnWave function and enemy factory

---
+++ Assets/Scripts/Entities/EnemyFactory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    private GameObject[] enemyTypes;
+
+    private GameObject[] spawnPoints;
+
+    private int enemyNumber = 0;
+    public int maxEnemyNumber = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        enemyTypes = new GameObject[] { enemy1, enemy2, enemy3 };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // return true if the enemy is spawned
+    public bool SpawnEnemy()
+    {
+        if (enemyNumber >= maxEnemyNumber) { return false; };
+        Vector3 spawnPosition = selectSpawnPosition();
+        GameObject newEnemy = Instantiate(selectEnemy(), spawnPosition, Quaternion.identity);
+        enemyNumber++;
+        return true;
+    }
+
+    private Vector3 selectSpawnPosition()
+    {
+        GameObject selectedSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Vector3 currentPoint = selectedSpawnPoint.transform.position;
+        currentPoint.y += .3f;
+        return currentPoint;
+    }
+    private GameObject selectEnemy()
+    {
+        GameObject selectedEnemy = enemyTypes[Random.Range(0, enemyTypes.Length)];
+        return selectedEnemy;
+    }
+
+    public void decreaseEnemyCounter()
+    {
+        enemyNumber -= 1;
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    int currentWave = 0;
+    float timeBetweenWaves = 5f;
+    float spawnRate = 0.5f;
+    bool stopSpawning = false;
+
+    public EnemyFactory enemyFactory;
+    private InGameCanvas inGameCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inGameCanvas = GameObject.Find("InGameCanvas").GetComponent<InGameCanvas>();
+        StartCoroutine(SpawnWave(10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inGameCanvas.UpdateWaveDisplay(currentWave);
+    }
+
+
+
+    IEnumerator SpawnWave(int enemyNumber)
+    {
+        int enemyCounter = 0;
+        while(enemyCounter < enemyNumber)
+        {
+            if(enemyFactory.SpawnEnemy())
+            {
+                enemyCounter++;
+            }
+            yield return new WaitForSeconds(spawnRate);
+
+        }
+        
+    }
+}
