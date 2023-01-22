7e74409d58cb721ecd61ce8fdb380e0747325b56
ElyGuill <ely-anneguillaume@outlook.fr>
2023-01-22 21:33:07 +0100

    Add arene

---
+++ Assets/Scripts/Fosse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fosse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    function OnCollisionEnter(Collision collision)
+ {
+     if( collision.gameObject.tag == "Player" )
+     {
+        collision.gameObject.health = 0;
+        
+     }
+ }
+}
